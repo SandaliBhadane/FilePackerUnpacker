3762db7c0048fc6f3fbb23a28767252832b36053
SandaliBhadane <sandalibhadane20@gmail.com>
2026-06-13 12:31:17 +0530

    Add files via upload

---
+++ 06_Packer/program604.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+
+class program604
+{
+  public static void main(String A[]) throws Exception
+  {
+    String Header = null;
+    byte Key = 0x11;
+
+    int iRet = 0;
+    int i = 0, j = 0;
+    byte Buffer[] = new byte[1024];
+
+    Scanner sobj = new Scanner(System.in);
+
+    System.out.println("Enter the name of folder :");
+    String FolderName = sobj.nextLine();
+
+    System.out.println("Enter the name of Packed file :");
+    String PackName = sobj.nextLine();
+
+    File fobj = new File(FolderName);
+
+    if(fobj.exists() && (fobj.isDirectory()))
+    {
+      File Packobj = new File(PackName);
+
+      Packobj.createNewFile();
+      FileOutputStream foobj = new FileOutputStream(Packobj);
+
+      FileInputStream fiobj = null;
+
+      System.out.println("Folder is present.");
+
+      File fArr[] = fobj.listFiles();
+
+      System.out.println("Number os Files in the folder : "+fArr.length);
+
+      for( i = 0 ; i<fArr.length; i++)
+      {
+        fiobj = new FileInputStream((fArr[i]));
+        
+        if(fArr[i].getName().endsWith(".txt"))
+        {
+          //Header formation
+          Header = fArr[i].getName() + " " +fArr[i].length();
+
+          for(j= Header.length();j<100;j++)
+          {
+            Header = Header+" ";
+          }
+
+          System.out.println("header :"+Header+"Length :"+Header.length());
+          /*  
+          while((iRet = fiobj.read(Buffer)) != -1)
+          {
+
+            //Encryption logic
+            for(j =0;j<iRet ; j++)
+            {
+              Buffer[j] = (byte)(Buffer[j] ^ Key);
+            }
+           foobj.write(Buffer,0,iRet);
+          }
+          */
+          
+        }
+         fiobj.close();
+       
+      }
+      foobj.close();
+    }
+    else
+    {
+      System.out.println("There is no such folder.");
+    }
+  }
+}
